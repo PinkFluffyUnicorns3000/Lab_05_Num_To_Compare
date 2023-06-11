87b160eb28e91287a09b4811d6cf1d5d02f32485
benhillerich <hillerbh@email.uc.edu>
2023-06-11 15:17:43 -0400

    Initial commit

---
+++ src/Num_To_Compare.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Num_To_Compare {
+    public static void main(String[] args)
+    {
+        int numOne = 0;
+        int numTwo = 0;
+
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is you number one? ");
+        if(in.hasNextInt()) {
+            numOne = in.nextInt();
+            in.nextLine();
+        }else
+        {
+            System.out.println("Your number is invalid! ");
+            System.exit(0);
+        }
+        System.out.print("What is your number two? ");
+        if(in.hasNextInt())
+        {
+            numTwo = in.nextInt();
+            in.nextLine();
+        }else
+        {
+            System.out.println("Your number is invalid! ");
+            System.exit(0);
+        }
+        if (numOne == numTwo)
+        {
+            System.out.println("The numbers are equal! " + numOne + ", " + numTwo);
+        } else if (numOne > numTwo)
+        {
+            System.out.println("Number one " + numOne + " is larger than number two " + numTwo  +".");
+        } else if (numOne < numTwo)
+        {
+            System.out.println("Number one " + numOne + " is less than number two " + numTwo + ".");
+        }
+    }
+}
